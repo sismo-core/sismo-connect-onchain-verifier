5fcf318c9ba2fc44318575521653c326ee1c6e39
leo Sayous <leo@sismo.io>
2023-03-20 15:09:51 +0100

    feat: add basic deployment script

---
+++ script/01_Deploy_zk-connect.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "../src/ZkConnectVerifier.sol";
+
+contract DeployZkConnect is Script {
+    ZkConnectVerifier zkConnectVerifier;
+
+    function run() external {
+        vm.startBroadcast();
+
+        zkConnectVerifier = new ZkConnectVerifier();
+
+        vm.stopBroadcast();
+    }
+}
