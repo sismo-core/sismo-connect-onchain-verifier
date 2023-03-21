89588336b1b7aebd0cdb5df5770e1588d688f0cc
bigq <yum0e@protonmail.com>
2023-03-21 19:14:11 +0100

    feat: add public inputs check, needs to add check on the destination

--- test/verifiers/hydra-s2/ZkConnectTest.t.sol
+++ test/verifiers/hydra-s2/ZkConnectTest.t.sol
@@ -8,12 +8,14 @@ import "src/libs/Struct.sol";
 contract ZkConnectTest is HydraS2BaseTest {
     function testZkConnect() public {
         bytes16 appId = 0x112a692a2005259c25f6094161007967;
-        bytes16 namespace = "main";
+        bytes16 groupId = 0xe9ed316946d3d98dfcd829a53ec9822e;
+        bytes16 groupTimestamp = bytes16('latest');
+        bytes memory namespace = bytes("main");
 
         StatementRequest[] memory statementRequests = new StatementRequest[](1);
         statementRequests[0] = StatementRequest({
-            groupId: 0x112a692a2005259c25f6094161007967,
-            groupTimestamp: 0x112a692a2005259c25f6094161007967,
+            groupId: groupId,
+            groupTimestamp: groupTimestamp,
             requestedValue: 0,
             comparator: StatementComparator.GTE,
             provingScheme: "hydra-s2.1",
@@ -22,6 +24,6 @@ contract ZkConnectTest is HydraS2BaseTest {
         DataRequest memory dataRequest =
             DataRequest({statementRequests: statementRequests, operator: LogicalOperator.AND});
 
-        zkConnectVerifier.verify(appId, hydraS2Proofs.getZkConnectResponse1(), dataRequest, namespace);
+        zkConnectVerifier.verify(appId, hydraS2Proofs.getZkConnectResponse1(), dataRequest, bytes16(keccak256(namespace)));
     }
 }
