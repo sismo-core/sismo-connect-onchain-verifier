357529b04be757f9ac645d53eb9734959d921c59
bigq <yum0e@protonmail.com>
2023-03-29 15:31:42 +0200

    feat: import interface instead of all zkConnectVerifier contract

---
+++ src/interfaces/IZkConnectVerifier.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "src/libs/utils/Struct.sol";
+
+interface IZkConnectVerifier {
+  function verify(ZkConnectResponse memory zkConnectResponse, ZkConnectRequestContent memory zkConnectRequestContent)
+        external
+        returns (ZkConnectVerifiedResult memory);
+
+  function ZK_CONNECT_VERSION() external view returns (bytes32);
+}
