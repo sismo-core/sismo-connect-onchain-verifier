dfe26abeebbba4df8f820fcc40453eb5521d1362
leo Sayous <leo@sismo.io>
2023-04-13 13:03:39 +0900

    feat: update default value for is selectable by user depending on the requested userId

--- src/libs/utils/AuthRequestBuilder.sol
+++ src/libs/utils/AuthRequestBuilder.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.17;
 import "src/libs/utils/Structs.sol";
 
 library AuthRequestBuilder {
-   // default values for Auth Request
+  // default values for Auth Request
   bool public constant DEFAULT_AUTH_REQUEST_IS_ANON = false;
   uint256 public constant DEFAULT_AUTH_REQUEST_USER_ID = 0;
   bool public constant DEFAULT_AUTH_REQUEST_IS_OPTIONAL = false;
-  bool public constant DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER = true;
   bytes public constant DEFAULT_AUTH_REQUEST_EXTRA_DATA = "";
+
+  error InvalidUserIdAndIsSelectableByUserAuthTypeParams();
 
   function build(
     AuthType authType,
@@ -19,75 +20,79 @@ library AuthRequestBuilder {
     bool isSelectableByUser,
     bytes memory extraData
   ) external pure returns (AuthRequest memory) {
-    return AuthRequest({
-      authType: authType, 
-      isAnon: isAnon, 
-      userId: userId, 
-      isOptional: isOptional,
-      isSelectableByUser: isSelectableByUser,
-      extraData: extraData
-    });
+    return
+      _build({
+        authType: authType,
+        isAnon: isAnon,
+        userId: userId,
+        isOptional: isOptional,
+        isSelectableByUser: isSelectableByUser,
+        extraData: extraData
+      });
   }
 
-  function build(AuthType authType, bool isAnon, uint256 userId, bytes memory extraData) external pure returns (AuthRequest memory) {
+  function build(
+    AuthType authType,
+    bool isAnon,
+    uint256 userId,
+    bytes memory extraData
+  ) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: isAnon,
         userId: userId,
         isOptional: DEFAULT_AUTH_REQUEST_IS_OPTIONAL,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: extraData
       });
   }
 
   function build(AuthType authType) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: DEFAULT_AUTH_REQUEST_IS_ANON,
         userId: DEFAULT_AUTH_REQUEST_USER_ID,
         isOptional: DEFAULT_AUTH_REQUEST_IS_OPTIONAL,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
       });
   }
 
   // we comment this function because it has the same signature as build(AuthType authType, bool isOptional)
   // but this one is not used for now since the anonymous auth is not implemented
-  
+
   // function build(AuthType authType, bool isAnon) external pure returns (AuthRequest memory) {
   //   return
-  //     AuthRequest({
+  //     _build({
   //       authType: authType,
   //       isAnon: isAnon,
   //       userId: DEFAULT_AUTH_REQUEST_USER_ID,
   //       isOptional: DEFAULT_AUTH_REQUEST_IS_OPTIONAL,
-  //       isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
   //       extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
   //     });
   // }
 
   function build(AuthType authType, uint256 userId) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: DEFAULT_AUTH_REQUEST_IS_ANON,
         userId: userId,
         isOptional: DEFAULT_AUTH_REQUEST_IS_OPTIONAL,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
       });
   }
 
-  function build(AuthType authType, bytes memory extraData) external pure returns (AuthRequest memory) {
+  function build(
+    AuthType authType,
+    bytes memory extraData
+  ) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: DEFAULT_AUTH_REQUEST_IS_ANON,
         userId: DEFAULT_AUTH_REQUEST_USER_ID,
         isOptional: DEFAULT_AUTH_REQUEST_IS_OPTIONAL,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: extraData
       });
   }
@@ -98,12 +103,11 @@ library AuthRequestBuilder {
     uint256 userId
   ) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: isAnon,
         userId: userId,
         isOptional: DEFAULT_AUTH_REQUEST_IS_OPTIONAL,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
       });
   }
@@ -114,12 +118,11 @@ library AuthRequestBuilder {
     bytes memory extraData
   ) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: isAnon,
         userId: DEFAULT_AUTH_REQUEST_USER_ID,
         isOptional: DEFAULT_AUTH_REQUEST_IS_OPTIONAL,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: extraData
       });
   }
@@ -130,29 +133,24 @@ library AuthRequestBuilder {
     bytes memory extraData
   ) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: DEFAULT_AUTH_REQUEST_IS_ANON,
         userId: userId,
         isOptional: DEFAULT_AUTH_REQUEST_IS_OPTIONAL,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: extraData
       });
   }
 
   // allow dev to choose for isOptional
 
-  function build(
-    AuthType authType,
-    bool isOptional
-  ) external pure returns (AuthRequest memory) {
+  function build(AuthType authType, bool isOptional) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: DEFAULT_AUTH_REQUEST_IS_ANON,
         userId: DEFAULT_AUTH_REQUEST_USER_ID,
         isOptional: isOptional,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
       });
   }
@@ -163,12 +161,11 @@ library AuthRequestBuilder {
     bool isOptional
   ) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: isAnon,
         userId: DEFAULT_AUTH_REQUEST_USER_ID,
         isOptional: isOptional,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
       });
   }
@@ -179,12 +176,11 @@ library AuthRequestBuilder {
     bool isOptional
   ) external pure returns (AuthRequest memory) {
     return
-      AuthRequest({
+      _build({
         authType: authType,
         isAnon: DEFAULT_AUTH_REQUEST_IS_ANON,
         userId: userId,
         isOptional: isOptional,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
         extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
       });
   }
@@ -196,13 +192,71 @@ library AuthRequestBuilder {
     bool isOptional
   ) external pure returns (AuthRequest memory) {
     return
+      _build({
+        authType: authType,
+        isAnon: isAnon,
+        userId: userId,
+        isOptional: isOptional,
+        extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
+      });
+  }
+
+  function _build(
+    AuthType authType,
+    bool isAnon,
+    uint256 userId,
+    bool isOptional,
+    bytes memory extraData
+  ) internal pure returns (AuthRequest memory) {
+    return
+      _build({
+        authType: authType,
+        isAnon: isAnon,
+        userId: userId,
+        isOptional: isOptional,
+        isSelectableByUser: _authIsSelectableDefaultValue(userId),
+        extraData: extraData
+      });
+  }
+
+  function _build(
+    AuthType authType,
+    bool isAnon,
+    uint256 userId,
+    bool isOptional,
+    bool isSelectableByUser,
+    bytes memory extraData
+  ) internal pure returns (AuthRequest memory) {
+    // When `userId` is 0, it means the app does not require a specific auth account and the user needs
+    // to choose the account they want to use for the app.
+    // When `isSelectableByUser` is true, the user can select the account they want to use.
+    // The combination of `userId = 0` and `isSelectableByUser = false` does not make sense and should not be used.
+    // If this combination is detected, the function will revert with an error.
+    if (userId == 0 && isSelectableByUser == false) {
+      revert InvalidUserIdAndIsSelectableByUserAuthTypeParams();
+    }
+    return
       AuthRequest({
         authType: authType,
         isAnon: isAnon,
         userId: userId,
         isOptional: isOptional,
-        isSelectableByUser: DEFAULT_AUTH_REQUEST_IS_SELECTABLE_BY_USER,
-        extraData: DEFAULT_AUTH_REQUEST_EXTRA_DATA
+        isSelectableByUser: isSelectableByUser,
+        extraData: extraData
       });
+  }
+
+  function _authIsSelectableDefaultValue(uint256 requestedUserId) internal pure returns (bool) {
+    // When `requestedUserId` is 0, it means no specific auth account is requested by the app,
+    // so we want the default value for `isSelectableByUser` to be `true`.
+    if (requestedUserId == 0) {
+      return true;
+    }
+    // When `requestedUserId` is not 0, it means a specific auth account is requested by the app,
+    // so we want the default value for `isSelectableByUser` to be `false`.
+    else {
+      return false;
+    }
+    // However, the dev can still override this default value by setting `isSelectableByUser` to `true`.
   }
 }
